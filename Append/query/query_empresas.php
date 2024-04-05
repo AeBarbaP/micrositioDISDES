c45417622a0f3f36f1fe9f09dbe3cd9dd6ccdeb9
AnnyBarba <aebarbap@gmail.com>
2024-04-05 15:27:47 -0600

    cards empresas listas

--- Append/query/query_empresas.php
+++ Append/query/query_empresas.php
@@ -4,26 +4,27 @@
     $resultadoSql = $conn3->query($sql);
     while($row = $resultadoSql->fetch_assoc()){
         echo'
-        <div class="col-lg-4">
+        <div class="col-lg-6 col-md-6">
             <div class="card mb-3" style="max-width: 540px;">
                 <div class="row g-0">
                     <div class="col-md-4 my-auto">
-                    <img src="assets/img/lazaritoDIV.png" class="img-fluid rounded-start p-2" alt="...">
+                        <img src="assets/logotipos/'.$row['logo'].'" class="img-fluid rounded-start p-2" alt="...">
                     </div>
                     <div class="col-md-8 bg-primary text-light text-center p-3">
-                    <div class="card-body">
-                        <h5 class="card-title">'.$row['nombre_rs'].'</h5>
-                        <p class="card-text"><small class="text-body-light">
-                        <a class="icon-link icon-link-hover text-light" style="--bs-link-hover-color-rgb: 25, 135, 84;" href="javascript: void(0)" onclick="abrirEmpresa('.$row['id'].')">
-                            <svg class="bi" aria-hidden="true"><use xlink:href="#link2"></use></svg>
-                            Ver perfil
-                        </a>
-                            </small></p>
+                        <div class="card-body">
+                            <h5 class="card-title">'.$row['nombre_rs'].'</h5>
+                            <p class="card-text">
+                                <small class="text-body-light">
+                                    <a class="icon-link icon-link-hover text-light" style="--bs-link-hover-color-rgb: 25, 135, 84;" href="javascript: void(0)" onclick="abrirEmpresa('.$row['id'].')">
+                                    <svg class="bi" aria-hidden="true"><use xlink:href="#link2"></use></svg>
+                                    Ver perfil</a>
+                                </small>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
         ';
     }
 ?>
